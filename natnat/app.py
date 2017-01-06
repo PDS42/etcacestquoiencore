286d5da812c443a39e6e1c1f8f0a4cb0cef3aef8
Dylan DJIAN <ddjian@e2r4p19.42.fr>
2017-01-06 16:11:04 +0100

    for poulet

---
+++ natnat/app.py
@@ -0,0 +1,48 @@
+import json
+
+entries = [
+    "DANS MON FIL D’ACTUALITE",
+    "C EST QUOI A DROITE DE MON FB",
+    "C EST QUOI A DROITE DE FIL D’ACTUALITE",
+    "VOUS VOUS FOUTEZ DE MA GUEULE ENCORE",
+
+
+
+]
+
+
+body = [
+    "ET TOUTES LES MAGOUILLES ORDURIERES",
+    "ET POUR ME POURRIR MA VIE EN TOUT POINT DE VU",
+    "ET PAR LE BIAIS DE N IMPORTE QUOI",
+    "ET MOI ET MON FILS ET LES ANIMAUX",
+    "ET POUR ME POURRIR MA VIE EN TOUT POINT DE VU",
+    "ET VOUS ET LES AUTRES DEBILES MENTAUX",
+    "ET JE MANGEAIS DU JAMBON DE DINDE ET DU PAIN",
+    "ET SALADE ET ON ETAIT AU TELEPHONE ET JE FAISAIS DU MENAGE ETC",
+    "ET TOUTES LES MAGOUILLES ORDURIERES",
+    "ET DE TOUT ORDRE ET DEPUIS DES ANNEES",
+    "ET POUR ME FOUTRE MA VIE EN L AIR ET CELLE DE MON FILS ET DES ANIMAUX ET D AUTRES AUSSI"
+
+
+
+]
+
+
+
+end = [
+    "ET DEPUIS DES ANNEES",
+    "ET TOUT CE QUI S’EN SUIT"
+
+
+]
+
+dict = {
+    'entries' : entries, 
+    'body' : body,
+    'end' : end
+}
+
+with open('data.txt', 'w', encoding='utf8') as outfile:
+    json.dump(dict, outfile, indent=4)
+outfile.close()
